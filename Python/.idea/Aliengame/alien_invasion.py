5b5495464be25e5fdc717f8d1ba3d3d45994044d
shencheng <2283228123@qq.com>
2020-09-20 11:51:52 +0800

    alien invasion

--- Python/.idea/Aliengame/alien_invasion.py
+++ Python/.idea/Aliengame/alien_invasion.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
+
 def run_game():
     pygame.init()
     ai_settings = Settings()
 
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
-    #screen = pygame.display.set_mode(200,200)
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    # screen = pygame.display.set_mode(200,200)
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
 
     while True:
 
@@ -16,6 +21,7 @@ def run_game():
                 sys.exit()
 
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 
